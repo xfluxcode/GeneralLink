5d49a69b1c0e89c8cc3a8065cf6c08ae28fb0dbd
xfluxcode <xfluxcode.id@gmail.com>
2017-09-22 21:10:26 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -21,10 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="the-way-to-start-business-at-home.html"}
-if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
-if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://kompisafelinkv2.blogspot.com/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="09/anyoption-review-forex-brokers-reviews.html"}
+if(x=="2"){xxx="09/forex-swing-trading-strategies.html"}
+if(x=="3"){xxx="09/forex-social-platforms-dailyforex.html"}
+if(x=="4"){xxx="09/how-to-build-winning-trading-plan.html"}
+if(x=="5"){xxx="09/trading-price-action-reversal-signals.html"}a_to_ve[i].href="https://generalink.blogspot.com/2017/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
